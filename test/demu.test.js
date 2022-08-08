159279bce251b2e00c9519ae8eb286b333973a71
Giacomo Brunetti <25010047+thundering-silence@users.noreply.github.com>
2022-08-08 11:38:16 +0200

    minimal tests

--- test/demu.test.js
+++ test/demu.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-expressions */
 const { expect } = require("chai");
-const { constants } = require("ethers");
+const { constants, utils } = require("ethers");
 const { ethers } = require("hardhat");
 require("dotenv").config();
 
@@ -21,6 +21,14 @@ describe("DEMU", function () {
 
     this.DEMU = await ethers.getContractFactory("Demu");
     this.demu;
+    this.WMATIC = new ethers.Contract(
+      process.env.WMATIC,
+      [
+        "function deposit(uint256 wad) public payable",
+        "function approve(address spender, uint256 amount) public",
+      ],
+      this.owner
+    );
   });
 
   it("should be deployable", async () => {
@@ -31,6 +39,50 @@ describe("DEMU", function () {
     );
     await this.demu.deployed();
     expect(this.demu.address).to.not.be.undefined;
+  });
+
+  it("should allow for supplying tokens", async () => {
+    const tx = await this.WMATIC.deposit(utils.parseEther("100"), {
+      value: utils.parseEther("100"),
+    });
+    await tx.wait();
+    await this.WMATIC.approve(this.demu.address, utils.parseEther("10"));
+    await this.demu.supply(process.env.WMATIC, utils.parseEther("10"));
+  });
+
+  it("should allow for querying for collateral Value", async () => {
+    const res = await this.demu.collateral(await this.owner.getAddress());
+    expect(res.gt(constants.WeiPerEther)).to.be.true;
+  });
+
+  it("should allow for minting DEMU", async () => {
+    const max = await this.demu.maxMintable(await this.owner.getAddress());
+    await this.demu.mint(max);
+    const demuBalance = await this.demu.balanceOf(
+      await this.owner.getAddress()
+    );
+    expect(demuBalance.eq(max)).to.be.true;
+  });
+
+  it("should prevent from withdrawing when minted amount is too big", async () => {
+    let err = false;
+    try {
+      await this.demu.withdraw(constants.WeiPerEther);
+    } catch (e) {
+      err = true;
+    }
+    expect(err).to.be.true;
+  });
+
+  it("should allow for burning DEMU", async () => {
+    const demuBalanceBefore = await this.demu.balanceOf(
+      await this.owner.getAddress()
+    );
+    await this.demu.burn(demuBalanceBefore);
+    const demuBalanceAfter = await this.demu.balanceOf(
+      await this.owner.getAddress()
+    );
+    expect(demuBalanceAfter.eq(constants.Zero)).to.be.true;
   });
 
   // it("should not allow minting by owner", async () => {
